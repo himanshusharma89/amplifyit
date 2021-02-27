5c198cc6da0a49fb4726992595ef20522f574791
Himanshu Sharma <ranubhardwaj89@gmail.com>
2021-02-27 22:59:13 +0530

    Constants

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,48 @@
+import 'package:amplifyit/helpers/launcher.dart';
+
+final Launcher launcher = Launcher();
+
+/// ROUTE CONSTANTS
+
+class RouteConstant {
+  static const ROOT = "/";
+  static const ADD_POST = "/add-post";
+  static const EDIT_POST = "/edit-post";
+  static const VIEW_POST = "/view-post";
+  static const ABOUT = "/about";
+  static const MEDIUM_ARTICLES = "/medium-articles";
+  static const MEDIUM_ARTICLES_WEB_VIEW = "/medium-article-web-view";
+}
+
+/// SOCIAL LINKS
+
+List social = [
+  {
+    'URL': 'https://github.com/himanshusharma89',
+    'iconURL': 'https://img.icons8.com/fluent/50/000000/github.png'
+  },
+  {
+    'URL': 'https://twitter.com/_SharmaHimanshu',
+    'iconURL': 'https://img.icons8.com/color/48/000000/twitter.png'
+  },
+  {
+    'URL': 'https://www.linkedin.com/in/himanshusharma89/',
+    'iconURL': 'https://img.icons8.com/color/48/000000/linkedin.png'
+  },
+];
+
+// var introSlider = [
+//   PageViewModel(
+//     text: "Do you have ideas that you want to pen down?",
+//     image: "Blog3.png",
+//   ),
+//   PageViewModel(
+//     text: "Looking for a spot to write blogs?",
+//     image: "Blog2.png",
+//   ),
+//   PageViewModel(
+//     text:
+//         "You came to the right place!\nWrite, read and even fetch articles from internet!",
+//     image: "Blog1.jpg",
+//   ),
+// ];
